a48a3f187156c2fa927c6547da14cbeeaef56538
Chang Yichun <e1354522@u.nus.edu>
2025-04-03 19:54:28 +0800

    Update Temp_Reading.py

--- Temp_Reading.py
+++ Temp_Reading.py
@@ -1,12 +1,21 @@
 import smbus2
 import time
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
 
-class MLX90614:
+class MLX90614(Node):
     def __init__(self):
+        super().__init__("tempsensor")
+        
         # Temperature Sensor (MLX90614)
         self.temp_sensor_address = 0x5A
         self.bus = smbus2.SMBus(1)  # I2C bus 1
         self.threshold = 40
+
+        self.temppub = self.create_publisher(Float32, '/temp_pub',10)
+        self.object_temp = Float32()
+        self.object_temp.data = 0.0
     
     def read_temp(self):
         def read_temp(register):
@@ -22,13 +31,13 @@ if __name__ == "__main__":
     try: 
         reader = MLX90614()
         while True:
-            object_temp, ambient_temp = self.read_temp()
-            print(f"Object Temp: {object_temp}°C | Ambient Temp: {ambient_temp}°C")
-
+            self.object_temp.data, ambient_temp = self.read_temp()
+            print(f"Object Temp: {self.object_temp.data}°C | Ambient Temp: {ambient_temp}°C")
+            self.temppub.publish(self.object_temp)
             if object_temp > threshold:
                 print("Object temperature too high! Launching...")
                 return "L" 
-            time.sleep(1)  # Wait 1 second before checking again
+            time.sleep(0.3)  # Wait 1 second before checking again
 
     except KeyboardInterrupt:
         print("Temperature reading stopped.")
