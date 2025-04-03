0ac7d81886282affaa000655c98f33174b1f6946
Chang Yichun <e1354522@u.nus.edu>
2025-04-03 21:06:59 +0800

    Update Temp_Reading.py

--- Temp_Reading.py
+++ Temp_Reading.py
@@ -1,45 +1,50 @@
 import smbus2
 import time
 import rclpy
+import RPi.GPIO as GPIO
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
-class MLX90614(Node):
+class Tempsens(Node):
     def __init__(self):
-        super().__init__("tempsensor")
+        super().__init__("HeatSensor")
         
         # Temperature Sensor (MLX90614)
-        self.temp_sensor_address = 0x5A
+        self.MLX90614_ADDR = 0x5A
+        self.MLX90614_TA = 0x06
+        self.MLX90614_TOBJ1 = 0x07
         self.bus = smbus2.SMBus(1)  # I2C bus 1
-        self.threshold = 40
-
+        
         self.temppub = self.create_publisher(Float32, '/temp_pub',10)
         self.object_temp = Float32()
         self.object_temp.data = 0.0
-    
-    def read_temp(self):
-        def read_temp(register):
-            data = self.bus.read_word_data(self.temp_sensor_address, register)
-            temp = (data * 0.02) - 273.15  # Convert to Celsius
-            return round(temp, 2)
+        
+    def read_temperature(self,register):
+        data = self.bus.read_i2c_block_data(self.MLX90614_ADDR, register,2)
+        raw_data = (data[1] << 8) + data[0]
+        temperature = (raw_data * 0.02) - 273.15
+        return temperature
+    def fn(self):
+        try:
+            while True:
+                ambient_temp = self.read_temperature(MLX90614_TA)
+                object_temp = self.read_temperature(MLX90614_TOBJ1)
 
-        object_temp = read_temp(0x07)  # Object temperature register
-        ambient_temp = read_temp(0x06)  # Ambient temperature register
-        return object_temp, ambient_temp  # Returns as a tuple
+                self.temp.data = object_temp
+                print(f"Ambient Temp: {ambient_temp:.2f} degrees Celsius")
+                print(f"Object Temp: {object_temp:.2f} degrees Celsius")
+                print(f"\n")
+                self.temppub.publish(Self,temp)
+                time.sleep(0.5)
+        except KeyboardInterrupt:
+            print("Exiting..")
+        finally:
+            GPIO.cleanup()
+        
 
 if __name__ == "__main__":
-    try: 
-        reader = MLX90614()
-        while True:
-            self.object_temp.data, ambient_temp = self.read_temp()
-            print(f"Object Temp: {self.object_temp.data}°C | Ambient Temp: {ambient_temp}°C")
-            self.temppub.publish(self.object_temp)
-            if object_temp > threshold:
-                print("Object temperature too high! Launching...")
-                return "L" 
-            time.sleep(0.3)  # Wait 1 second before checking again
-
-    except KeyboardInterrupt:
-        print("Temperature reading stopped.")
-
-  
+    rclpy.init()
+    tempclass = Tempsens()
+    tempclass.fn()
+    tempclass.destroy_node()
+    rclpy.shutdown()
